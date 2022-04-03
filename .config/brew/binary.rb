3321bfd49443a5d75c407bc927124725df852a88
Megabyte Labs <help@megabyte.space>
2022-04-03 03:27:21 -0400

    ⤵️ automation(synchronize) Applying changes from upstream repository.

--- .config/brew/binary.rb
+++ .config/brew/binary.rb
@@ -1,7 +1,7 @@
 class AnsibleKeyring < Formula
   desc "A CLI that extends ansible, ansible-playbook, and ansible-vault to use the system keyring for vault passwords"
   homepage "https://megabyte.space"
-  url "https://github.com/ProfessorManhattan/ansible-keyring/releases/download/v1.2.7/ansible-keyring.tar.gz"
+  url "https://github.com/megabyte-labs/ansible-keyring/releases/download/v1.2.7/ansible-keyring.tar.gz"
   version "1.2.7"
   license "MIT"
 
